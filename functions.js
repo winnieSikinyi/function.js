16a8251ec0830fb1c7060926d3661c09b989420f
winnieSikinyi <winniesikinyi@gmail.com>
2023-04-17 20:54:32 +0300

    first commit

---
+++ functions.js
@@ -0,0 +1,13 @@
+//You manage a grocery store and need to keep track of the inventory 
+//of various items. You will use arrays to store the data and various 
+//functions to manipulate and analyze the data. 
+//Create an array containing the names of all items in the inventory maximum of 10.
+//Create a separate array with the corresponding stock quantities of each item maximum of 10.
+//Write a function to add a new item to the inventory, updating both arrays.
+//Write a function to update the stock quantity of an existing item.
+//Write a function to calculate the total number of items in the inventory.
+//Write a function to find the item with the lowest stock quantity.
+
+function grocery(){
+    
+}
